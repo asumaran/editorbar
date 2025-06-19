2e8a57cd4c156aa9bd7cf5a3be9d18718c3f7f7f
Alfredo Sumaran <a.sumaran@gmail.com>
2025-06-19 15:51:51 -0500

    Use real labels

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,10 @@ function App() {
   return (
     <div className='flex items-center justify-center h-screen'>
       <PageBar>
-        <PageBarItem>item 1</PageBarItem>
-        <PageBarItem>item 2</PageBarItem>
-        <PageBarItem>item 3</PageBarItem>
-        <PageBarItem>item 4</PageBarItem>
-        <PageBarItem>item 5</PageBarItem>
-        <PageBarItem>item 6</PageBarItem>
+        <PageBarItem>Info</PageBarItem>
+        <PageBarItem>Details</PageBarItem>
+        <PageBarItem>Other</PageBarItem>
+        <PageBarItem>Ending</PageBarItem>
       </PageBar>
       <PageSettingsMenu></PageSettingsMenu>
     </div>
